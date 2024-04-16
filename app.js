a9270b72b44bef11cc843d475fe9cafb0c08fe04
Tin <93732406+Tin0709@users.noreply.github.com>
2024-04-16 17:51:02 +0700

    Experiment with if statements

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+const name = "Tin";
+const age = 2;
+let addS = "";
 
+if (age > 1) {
+  let addS = "s";
+  console.log(`my name is ${name}, I'm ${age} year${addS} old`);
+} else {
+  let addS = "";
+  console.log(`my name is ${name}, I'm ${age} year${addS} old`);
+}
